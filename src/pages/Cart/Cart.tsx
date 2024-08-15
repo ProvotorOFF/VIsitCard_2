520562cb12d75f48ef017eecd54cbaf49a8140af
office-not-found <109953384+office-not-found@users.noreply.github.com>
2024-08-15 22:57:34 +0300

    Fit: итого в корзине

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,10 +8,18 @@ import axios from 'axios'
 import { PREFIX } from '../../helpers/API'
 import styles from './Cart.module.css'
 
+const DELIVERY_FEE = 169
+
 export default function Cart() {
 
 	const items = useSelector((s: RootState) => s.cart.items)
 	const [cartProducts, setCartProducts] = useState<Product[]>([])
+
+	const total = items.map(i => {
+		const product = cartProducts.find(p => p.id == i.id)
+		if (!product) return 0
+		return i.count * product.price
+	}).reduce((acc, i) => acc += i)
 
 	const getItem = async (id: number) => {
 		const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`)
@@ -20,13 +28,13 @@ export default function Cart() {
 
 	const loadAllItems = async () => {
 		const res = await Promise.all(items.map(i => getItem(i.id)))
-		
+
 		setCartProducts(res)
 	}
 
 	useEffect(() => {
 		loadAllItems()
-		
+
 	}, [items])
 
 	return <>
@@ -35,5 +43,19 @@ export default function Cart() {
 			const product = cartProducts.find(p => p.id == i.id)
 			return product && <CartItem key={i.id} count={i.count} {...product} />
 		})}
+		<div className={styles['line']}>
+			<div className={styles['text']}>Итог</div>
+			<div className={styles['price']}>{total}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles['hr']}/>
+		<div  className={styles['line']}>
+			<div className={styles['text']}>Доставка</div>
+			<div className={styles['price']}>{DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles['hr']}/>
+		<div className={styles['line']}>
+			<div className={styles['text']}>Итог {items.length}</div>
+			<div className={styles['price']}>{total + DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
 	</>
 }
